c153b44c95dd7f6de7c7668751784ad01bf82d91
SohamDahanukar <sohamdahanukar@gmail.com>
2023-12-20 11:19:40 +0530

    Commit

---
+++ Soham_SDP-main/Loan/Loan.java
@@ -0,0 +1,9 @@
+         
+           class Loan {
+             public double calculateEMI(double principal) {
+               double simpleInterest = (principal*8.5*5) / 100;
+               return (simpleInterest+principal)/5;
+             }
+           }
+            
+          
